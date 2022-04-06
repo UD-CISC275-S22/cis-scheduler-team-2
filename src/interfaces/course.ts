f6e84984cf4f7df2a10f4f2cd0380554a647bf51
Ryan Evans <ryancev@udel.edu>
2022-04-06 19:55:23 -0400

    Created course interface with initial details

---
+++ src/interfaces/course.ts
@@ -0,0 +1,13 @@
+/**
+ * Represents a single course
+ */
+export interface Course {
+    /** The code for the course */
+    courseCode: number;
+    /** The name of the course */
+    title: string;
+    /** The amount of credits the course is worth */
+    credits: number;
+    /** An Array containing the courses that are prereqs of this course */
+    prereqs: Course[];
+}
