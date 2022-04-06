de1430e3d632edbdfd86020c608a3bc44755a989
Craig Barber <craig@wifi-roaming-128-4-118-245.wifi.udel.edu>
2022-04-06 17:45:39 -0400

    Addition of interfaces folder for plan, semester, and course interfaces. - Plan interface file created, shell of interface contained within.

---
+++ src/interfaces/plan.ts
@@ -0,0 +1,11 @@
+/***
+ * A representation of a students' term
+ */
+export interface Plan {
+    /** The user-defined name of the plan */
+    name: string;
+    /** An array of semesters, representing all the planned semesters 
+    semesters: Semester[] */
+    /** The id of a plan, generated upon creation  */
+    id: number;
+}
