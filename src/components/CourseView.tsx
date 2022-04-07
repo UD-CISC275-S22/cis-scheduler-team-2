a0dd3bd9f7a88db19a9a967617ac537bc42b6b0d
Ryan Evans <ryancev@udel.edu>
2022-04-07 14:27:01 -0400

    Displays minimal course info

--- src/components/CourseView.tsx
+++ src/components/CourseView.tsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Course } from "../interfaces/course";
 
-export function CourseView(): JSX.Element {
-    return <div></div>;
+export function CourseView({ course }: { course: Course }): JSX.Element {
+    return (
+        <div>
+            <hr></hr>
+            <Container>
+                <Row>
+                    <Col>
+                        <Row>
+                            <h3>
+                                {course.department} {course.courseCode}:{" "}
+                                {course.title}
+                            </h3>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <h4>({course.credits} credits)</h4>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col></Col>
+                            <Col>
+                                <p>{course.description}</p>
+                            </Col>
+                            <Col></Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
