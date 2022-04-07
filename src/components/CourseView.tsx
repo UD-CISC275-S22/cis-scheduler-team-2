a62daa494c43e4a6618279d4422fcc17f92b1f1b
Ryan Evans <ryancev@udel.edu>
2022-04-07 14:47:54 -0400

    Course names are clickable to display description

--- src/components/CourseView.tsx
+++ src/components/CourseView.tsx
@@ -1,35 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Course } from "../interfaces/course";
 
 export function CourseView({ course }: { course: Course }): JSX.Element {
+    const [descriptionVis, setDescriptionVis] = useState<boolean>(false);
+
+    function flipVis(): void {
+        setDescriptionVis(!descriptionVis);
+    }
+
     return (
-        <div>
-            <hr></hr>
-            <Container>
-                <Row>
-                    <Col>
-                        <Row>
-                            <h3>
-                                {course.department} {course.courseCode}:{" "}
-                                {course.title}
-                            </h3>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <h4>({course.credits} credits)</h4>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col></Col>
-                            <Col>
-                                <p>{course.description}</p>
-                            </Col>
-                            <Col></Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </Container>
+        <div style={{ justifyContent: "center", display: "flex" }}>
+            <div style={{ width: "50%" }}>
+                <hr></hr>
+                <Container>
+                    <Row>
+                        <Col>
+                            <Row>
+                                <h3
+                                    onClick={flipVis}
+                                    style={{ cursor: "pointer" }}
+                                >
+                                    {course.department} {course.courseCode}:{" "}
+                                    {course.title}
+                                </h3>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    {descriptionVis && (
+                                        <h4>({course.credits} credits)</h4>
+                                    )}
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    {descriptionVis && (
+                                        <p>{course.description}</p>
+                                    )}
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
