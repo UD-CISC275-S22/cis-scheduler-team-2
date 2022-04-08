152313acd3d6a1172fd3620ebbfb6633525be860
Joshua Nicholls <jnich@udel.edu>
2022-04-07 20:14:35 -0400

    completed functionality for adding new plan to a list of plans

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { CourseList } from "./components/CourseList";
+import { Plan } from "./interfaces/plan";
+import { AddNewPlan } from "./components/AddNewPlan";
 
 function App(): JSX.Element {
+    const [planList, updatePlans] = useState<Plan[]>([]);
+
+    function addPlan(newPlan: Plan) {
+        const fixId = {
+            ...newPlan,
+            id: planList.length,
+            semesters: [...newPlan.semesters]
+        };
+        updatePlans([...planList, fixId]);
+    }
+
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <AddNewPlan addPlan={addPlan}></AddNewPlan>
             <CourseList></CourseList>
             <hr></hr>
             <p>
