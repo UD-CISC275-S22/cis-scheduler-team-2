213c12505a8277b6917f84859414e61ebba041fc
Ryan Evans <ryancev@udel.edu>
2022-04-11 22:55:07 -0400

    Added additional semester to sample plan

--- src/interfaces/placeholderPlan.ts
+++ src/interfaces/placeholderPlan.ts
@@ -16,9 +16,41 @@ const samplePlan: Plan = {
                     credits: 3,
                     prereqs: ["CISC181", "CISC210"],
                     description: "Basically web development"
+                },
+                {
+                    department: "CISC",
+                    courseCode: 320,
+                    title: "Introduction to Algorithms",
+                    credits: 3,
+                    prereqs: ["CISC181", "CISC210"],
+                    description: "Computers go burr"
                 }
             ],
-            credits: 3
+            credits: 6
+        },
+        {
+            id: 1,
+            season: "Spring",
+            year: 2020,
+            classes: [
+                {
+                    department: "CISC",
+                    courseCode: 437,
+                    title: "Introduction to Database Systems",
+                    credits: 3,
+                    prereqs: ["CISC181", "CISC210"],
+                    description: "SQL go burr"
+                },
+                {
+                    department: "CISC",
+                    courseCode: 482,
+                    title: "Artificial Intelligence",
+                    credits: 3,
+                    prereqs: ["CISC181", "CISC210"],
+                    description: "Computers be thinkin"
+                }
+            ],
+            credits: 6
         }
     ]
 };
