50023349adab908f9fbf8edc81465dab9ce30741
Ryan Evans <ryancev@udel.edu>
2022-04-11 22:53:50 -0400

    Added visibility toggle

--- src/components/CourseTable.tsx
+++ src/components/CourseTable.tsx
@@ -1,34 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table } from "react-bootstrap";
 import { Course } from "../interfaces/course";
 import { Semester } from "../interfaces/semester";
 
 export function CourseTable({ semester }: { semester: Semester }): JSX.Element {
+    const [isVisible, setIsVisible] = useState<boolean>(true);
+
+    function toggleVis(): void {
+        setIsVisible(!isVisible);
+    }
+
     return (
         <div>
             <div>
-                <h4>
+                <h4 onClick={toggleVis} style={{ cursor: "pointer" }}>
                     Semester: {semester.season} {semester.year}
                 </h4>
             </div>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Credits</th>
-                        <th>Course Number</th>
-                        <th>Course Title</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {semester.classes.map((course: Course) => (
-                        <tr key={course.courseCode}>
-                            <th>{course.credits}</th>
-                            <th>{`${course.department}${course.courseCode}`}</th>
-                            <th>{course.title}</th>
+            {isVisible && (
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Credits</th>
+                            <th>Course Number</th>
+                            <th>Course Title</th>
                         </tr>
-                    ))}
-                </tbody>
-            </Table>
+                    </thead>
+                    <tbody>
+                        {semester.classes.map((course: Course) => (
+                            <tr key={course.courseCode}>
+                                <th>{course.credits}</th>
+                                <th>{`${course.department}${course.courseCode}`}</th>
+                                <th>{course.title}</th>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            )}
         </div>
     );
 }
