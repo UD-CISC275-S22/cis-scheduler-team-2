ccb92602cac9235acb5fd996372bba900d231a89
Joshua Nicholls <jnich@udel.edu>
2022-04-14 14:55:15 -0400

    can clear individual semester. Function passed from App to SemesterTalbe to ClearSemester

---
+++ src/components/ClearSemester.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+//import { Plan } from "../interfaces/plan";
+import { Semester } from "../interfaces/semester";
+import { SemesterTable } from "./SemesterTable";
+
+interface clearSemesterProp {
+    PlanID: number;
+    thisSem: Semester;
+    clearFunct: (planID: number, semYear: number, semSeas: string) => void;
+}
+
+export function ClearSemesterButton({
+    PlanID,
+    thisSem,
+    clearFunct
+}: clearSemesterProp) {
+    return (
+        <div>
+            <Button
+                onClick={() => clearFunct(PlanID, thisSem.year, thisSem.season)}
+            >
+                Clear This Semester
+            </Button>
+        </div>
+    );
+}
