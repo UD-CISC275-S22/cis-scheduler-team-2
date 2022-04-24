5c27250430b48f81834146e1c7394e7ef81fd9df
Craig Barber <RacgiMan@icloud.com>
2022-04-24 05:16:29 -0400

    Added prereqsFilled and degreeReqsFilled fields to course interface & Added them to existing definitions for compatability.

--- src/interfaces/placeholderPlan.ts
+++ src/interfaces/placeholderPlan.ts
@@ -15,7 +15,9 @@ const samplePlan: Plan = {
                     title: "Introduction to Software Engineering",
                     credits: 3,
                     prereqs: ["CISC181", "CISC210"],
-                    description: "Basically web development"
+                    description: "Basically web development",
+                    prereqsFilled: [],
+                    degreeReqsFilled: []
                 },
                 {
                     department: "CISC",
@@ -23,7 +25,9 @@ const samplePlan: Plan = {
                     title: "Introduction to Algorithms",
                     credits: 3,
                     prereqs: ["CISC181", "CISC210"],
-                    description: "Computers go burr"
+                    description: "Computers go burr",
+                    prereqsFilled: [],
+                    degreeReqsFilled: []
                 }
             ],
             credits: 6
@@ -39,7 +43,9 @@ const samplePlan: Plan = {
                     title: "Introduction to Database Systems",
                     credits: 3,
                     prereqs: ["CISC181", "CISC210"],
-                    description: "SQL go burr"
+                    description: "SQL go burr",
+                    prereqsFilled: [],
+                    degreeReqsFilled: []
                 },
                 {
                     department: "CISC",
@@ -47,7 +53,9 @@ const samplePlan: Plan = {
                     title: "Artificial Intelligence",
                     credits: 3,
                     prereqs: ["CISC181", "CISC210"],
-                    description: "Computers be thinkin"
+                    description: "Computers be thinkin",
+                    prereqsFilled: [],
+                    degreeReqsFilled: []
                 }
             ],
             credits: 6
