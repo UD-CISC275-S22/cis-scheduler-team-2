62ecb463a75a7a40f26f60e3a401e91a589414ee
Ryan Evans <ryancev@udel.edu>
2022-04-24 18:01:03 -0400

    Populated course pool with all cisc classes

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -4,32 +4,42 @@ import { Plan } from "../interfaces/plan";
 import { Semester } from "../interfaces/semester";
 import { CourseView } from "./CourseView";
 
-export function CourseList({ plan }: { plan: Plan }): JSX.Element {
-    /** Array with the passsed in plan's semesters */
-    const PLAN_SEMESTERS: Semester[] = plan.semesters.map(
-        (semester: Semester) => semester
-    );
-
-    /** Array containing the courses array of every semester */
-    const SEM_COURSES: Course[][] = PLAN_SEMESTERS.map(
-        (semester: Semester) => semester.classes
-    );
-
-    /** Creating an array containing every course from every semester in the plan (Only if there are semesters in the plan) */
-    if (SEM_COURSES.length > 0) {
-        const allCourses: Course[] = SEM_COURSES.reduce(
-            (currResult, currItem) => currResult.concat(currItem)
-        );
+export function CourseList({
+    plan,
+    moveCourseFromPool
+}: {
+    plan: Plan;
+    moveCourseFromPool: (courseToMove: Course, toSemester: Semester) => void;
+    moveCourseToPool: (courseToMove: Course, fromSemester: Semester) => void;
+}): JSX.Element {
+    /** Creating an array containing every course from every semester in the plan (Only if there are semesters in the course pool) */
+    if (plan.coursePool.length > 0) {
         return (
-            <div>
+            <div style={{ paddingLeft: "10px", paddingRight: "10px" }}>
                 <h4>Course Pool</h4>
-                <ul style={{ listStyle: "none", paddingLeft: "0" }}>
-                    {allCourses.map((course: Course) => (
-                        <li key={course.courseCode}>
-                            <CourseView course={course}></CourseView>
-                        </li>
-                    ))}
-                </ul>
+                <div
+                    style={{
+                        overflowY: "scroll",
+                        height: "80vh",
+                        width: "auto",
+                        border: "solid",
+                        borderWidth: "1px",
+                        borderRadius: "8px",
+                        borderColor: "gray"
+                    }}
+                >
+                    <ul style={{ listStyle: "none", paddingLeft: "0" }}>
+                        {plan.coursePool.map((course: Course) => (
+                            <li key={course.courseCode}>
+                                <CourseView
+                                    course={course}
+                                    plan={plan}
+                                    moveCourseFromPool={moveCourseFromPool}
+                                ></CourseView>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         );
     }
