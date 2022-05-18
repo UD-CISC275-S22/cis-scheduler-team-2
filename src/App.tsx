86805bc1de8e535ab2330b002dc40ed5795b2aa2
Ryan Evans <ryancev@udel.edu>
2022-05-17 22:59:12 -0400

    Minor bug fix for course filtering

--- src/App.tsx
+++ src/App.tsx
@@ -438,7 +438,7 @@ function App(): JSX.Element {
                 semesters: activePlan.semesters,
                 coursePool: originalCoursePool,
                 originalCoursePool: activePlan.originalCoursePool,
-                activeFilters: [],
+                activeFilters: [...localFilterCopy],
                 currentDeptFilter: activePlan.currentDeptFilter,
                 degree: activePlan.degree,
                 filledRequirements: activePlan.filledRequirements
@@ -446,7 +446,7 @@ function App(): JSX.Element {
             const fixedPlanList = planList.map((plan: Plan) =>
                 plan.id === activePlan.id ? { ...fixedPlan } : { ...plan }
             );
-            console.log(`Currently applied filters: ${localFilterCopy}`);
+            console.log(`Currently APPLIED filters: ${localFilterCopy}`);
             setActivePlan(fixedPlan);
             updatePlans(fixedPlanList);
         }
